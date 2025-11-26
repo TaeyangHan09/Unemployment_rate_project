c61f571968afb1e2057ab9af21099112799d6285
shawnhan <shawnhan090508@icloud.com>
2025-11-25 20:29:15 -0500

    added secret API_KEY

--- main.py
+++ main.py
@@ -2,16 +2,22 @@ import os
 import streamlit as st
 import requests
 
-API_KEY = os.environ["API_KEY"]
+API_KEY = st.secrets["API_KEY"]
 headers = {"X-API-KEY": API_KEY}
 
-country = input("Enter country name: ")
-useryear = int(input("Enter year: "))
-url = f"https://api.api-ninjas.com/v1/unemployment?country={country}"
+st.title("Unemployment Rate Per Year per Country")
+st.write("Given country and a year, display unemployment rate")
+st.divider()
 
-request = requests.get(url, headers)
-response = request.json()
+country = st.text_input("Country: ")
+useryear = st.number_input("Year: ", step = 1, min_value = 1980, max_value =2029)
+submit = st.button("Submit")
+if submit:
+    url = f"https://api.api-ninjas.com/v1/unemployment?country={country}"
 
-for item in response:
-    if item["year"] == useryear:
-        print(item["unemployment_rate"])
+    request = requests.get(url, headers)
+    response = request.json()
+
+    for item in response:
+        if item["year"] == useryear:
+            st.subheader(f"THe unemployment rate is {item['unemployment_rate']}%")
